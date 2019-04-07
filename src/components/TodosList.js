a5066a9618342974683eefd6ec3e87a3e124d48c
Mohammed slimani <slimani.med.21@gmail.com>
2019-04-07 15:45:27 +0100

    Connected TodoLists to the backend.

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const URL = "http://localhost:4000/todos";
+
+const Todo = props => (
+  <tr>
+    <td>{props.todo.todo_description}</td>
+    <td>{props.todo.todo_priority}</td>
+    <td>
+      <Link to={"edit/" + props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+);
 
 export class TodosList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(URL)
+      .then(res => {
+        this.setState({
+          todos: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  todoList() {
+    return this.state.todos.map((currentTodo, index) => {
+      return <Todo todo={currentTodo} key={index} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>This is THe list of the Todos list</p>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Priority</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoList()}</tbody>
+        </table>
       </div>
     );
   }
