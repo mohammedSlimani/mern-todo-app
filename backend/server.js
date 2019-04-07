ab50fc53b5ecf3003f32ff2726ae509205ed61e6
Mohammed slimani <slimani.med.21@gmail.com>
2019-04-07 14:30:40 +0100

    Added EndPoints and Tested Api with Insomnia. BackEnd Success!

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
 
-app.use(cors);
+let Todo = require("./todoSchema");
+
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
@@ -14,6 +16,61 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("DATABASE Connection successfely established");
 });
+
+//Endpoints
+const todoRoutes = express.Router();
+
+todoRoutes.route("/").get((req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route("/:id").get((req, res) => {
+  let id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route("/add").post((req, res) => {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: "Todo Added Succefuly" });
+    })
+    .catch(err => {
+      res.status(400).send("Adding todo Failed");
+    });
+});
+
+todoRoutes.route("/update/:id").post((req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (!todo) {
+      res.status(404).send("Todo not found");
+    } else {
+      todo.todo_description = req.body.todo_description;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
+
+      todo
+        .save()
+        .then(todo => {
+          res.json("Todo Updated");
+        })
+        .catch(err => {
+          res.status(400).send("Error saving");
+        });
+    }
+  });
+});
+app.use("/todos", todoRoutes);
+
 app.listen(PORT, () => {
   console.log("App running on PORT: " + PORT);
 });
